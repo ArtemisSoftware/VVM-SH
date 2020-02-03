aa8c9ba533755ef3a0e202f2ff4976e5054c3a67
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-02-03 19:10:06 +0000

    layout ApresentaçãoActivity

---
+++ app/src/main/java/com/vvm/sh/apresentacao/ApresentacaoActivity.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.apresentacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class ApresentacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apresentacao);
+    }
+}

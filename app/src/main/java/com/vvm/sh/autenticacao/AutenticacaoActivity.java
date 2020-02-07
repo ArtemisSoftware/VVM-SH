77ca9845b4df28557a970966aec701d3389e4d27
gustavo <gustavo.maia@b2b.com.pt>
2020-02-07 16:20:27 +0000

    Shared preferences

--- app/src/main/java/com/vvm/sh/autenticacao/AutenticacaoActivity.java
+++ app/src/main/java/com/vvm/sh/autenticacao/AutenticacaoActivity.java
@@ -2,6 +2,8 @@ package com.vvm.sh.autenticacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.vvm.sh.R;
@@ -12,5 +14,15 @@ public class AutenticacaoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autenticacao);
+    }
+
+
+    private void iniciarApp(){
+
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", Context.MODE_PRIVATE); // 0 - for private mode
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("token", "string value");
+        editor.commit();
     }
 }

96dfba157309b25096a723836c7ea1daac9ed3a4
gustavo <gustavo.maia@b2b.com.pt>
2020-02-11 17:32:12 +0000

    Dependencia textdrawable

--- app/src/main/java/com/vvm/sh/util/Preferencias.java
+++ app/src/main/java/com/vvm/sh/util/Preferencias.java
@@ -3,6 +3,8 @@ package com.vvm.sh.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.vvm.sh.BuildConfig;
+
 public class Preferencias {
 
 
@@ -14,16 +16,15 @@ public class Preferencias {
      * Metodo que permite fixar os dados da primeira utilizacao da app
      * @param contexto
      * @param primeiraUtilizacao boolean que indica se é a primeira utilizacao da app
-     * @param versaoApp a versão da app
      */
-    public static void fixarPrimeiraUtilizacao(Context contexto, boolean primeiraUtilizacao, String versaoApp){
+    public static void fixarPrimeiraUtilizacao(Context contexto, boolean primeiraUtilizacao){
 
         String pacote = contexto.getPackageName();
 
         SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE); // 0 - for private mode
         SharedPreferences.Editor editor = preferencias.edit();
         editor.putBoolean(PRIMEIRA_UTILIZACAO, primeiraUtilizacao);
-        editor.putString(VERSAO_APP, versaoApp);
+        editor.putString(VERSAO_APP, BuildConfig.VERSION_NAME);
         editor.commit();
     }
 
@@ -40,5 +41,23 @@ public class Preferencias {
         SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE);
         return preferencias.getBoolean(PRIMEIRA_UTILIZACAO, true);
     }
+
+
+
+    /**
+     * Metodo que permite indetificar se é necessario realizar a apresentacao da versão da app
+     * @param contexto
+     * @return true caso seja necessário ou false caso contrario
+     */
+    public static boolean realizarApresentacaoApp(Context contexto) {
+
+        String pacote = contexto.getPackageName();
+
+        SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE);
+        String versao = preferencias.getString(VERSAO_APP, "0.0.0");
+
+        return !(BuildConfig.VERSION_NAME.equals(versao));
+    }
+
 
 }

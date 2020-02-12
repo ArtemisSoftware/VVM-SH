dfc7e3830c4fbc75b0483c3f84b791dc2494aab2
gustavo <gustavo.maia@b2b.com.pt>
2020-02-12 09:50:43 +0000

    Layout activity_tarefa Parte 1

---
+++ app/src/main/java/com/vvm/sh/ui/agenda/TarefaActivity.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.ui.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class TarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tarefa);
+    }
+}

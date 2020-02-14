e4e0a13348b7ab18d93a7999799700bab554e196
gustavo <gustavo.maia@b2b.com.pt>
2020-02-14 15:03:02 +0000

    Layout activity_atividades_executadas

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesExecutadas/AtividadesExecutadasActivity.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.ui.atividadesExecutadas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class AtividadesExecutadasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atividades_executadas);
+    }
+}

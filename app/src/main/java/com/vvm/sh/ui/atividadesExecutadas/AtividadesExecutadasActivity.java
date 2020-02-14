e8a6bb61e361d1e36ca910905f32b87de2be7fda
gustavo <gustavo.maia@b2b.com.pt>
2020-02-14 18:10:03 +0000

    AtividadesExecutadasActivity integrados com o recycler

--- app/src/main/java/com/vvm/sh/ui/atividadesExecutadas/AtividadesExecutadasActivity.java
+++ app/src/main/java/com/vvm/sh/ui/atividadesExecutadas/AtividadesExecutadasActivity.java
@@ -1,16 +1,72 @@
 package com.vvm.sh.ui.atividadesExecutadas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.vvm.sh.R;
+import com.vvm.sh.ui.BaseActivity;
+import com.vvm.sh.ui.agenda.Tarefa;
+import com.vvm.sh.ui.agenda.adaptadores.TarefaRecyclerAdapter;
+import com.vvm.sh.util.adaptadores.Item;
 
-public class AtividadesExecutadasActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+public class AtividadesExecutadasActivity extends BaseActivity {
+
+
+    @BindView(R.id.rcl_registos)
+    RecyclerView rcl_registos;
+
+
+    private AtividadeExecutadaRecyclerAdapter atividadeExecutadaRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividades_executadas);
+
+        iniciarAtividade();
+        obterRegistos();
+    }
+
+    //------------------------
+    //Metodos locais
+    //------------------------
+
+
+    /**
+     * Metodo que permite iniciar a atividade
+     */
+    private void iniciarAtividade(){
+
+        atividadeExecutadaRecyclerAdapter = new AtividadeExecutadaRecyclerAdapter();
+        rcl_registos.setAdapter(atividadeExecutadaRecyclerAdapter);
+        rcl_registos.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+    private void obterRegistos(){
+
+        List<Item> t1 = new ArrayList<>();
+        t1.add(new AtividadeExecutada(1, "Atividade numero 1", "29-10-2020"));
+        t1.add(new AtividadeExecutada(2, "Atividade numero 2", "30-10-2020"));
+
+        atividadeExecutadaRecyclerAdapter.fixarRegistos(t1);
+    }
+
+    /**
+     * Metodo que permite subscrever observadores
+     */
+    private void subscreverObservadores(){
+
+
+        //TODO: subscrever observadores do viewmodel
+
     }
 }
--- app/src/main/java/com/vvm/sh/util/adaptadores/ItemRecyclerAdapter.java
+++ app/src/main/java/com/vvm/sh/util/adaptadores/ItemRecyclerAdapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.vvm.sh.ui.atividadesExecutadas.AtividadeExecutada;
+
 import java.util.ArrayList;
 import java.util.List;
 

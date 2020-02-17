11ab455d3f829d5bf5b463097766b38d73909673
gustavo <gustavo.maia@b2b.com.pt>
2020-02-17 16:16:41 +0000

    Layout activity_cross_selling

---
+++ app/src/main/java/com/vvm/sh/ui/crossSelling/CrossSellingActivity.java
@@ -0,0 +1,17 @@
+package com.vvm.sh.ui.crossSelling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+import com.vvm.sh.ui.BaseActivity;
+
+public class CrossSellingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cross_selling);
+    }
+}

5e6ed4127a660af32e510e933e8958d7a6a25efb
gustavo <gustavo.maia@b2b.com.pt>
2020-02-20 14:59:18 +0000

    Layout activity_ocorrencias

---
+++ app/src/main/java/com/vvm/sh/ui/ocorrencias/OcorrenciasActivity.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.ui.ocorrencias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class OcorrenciasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocorrencias);
+    }
+}

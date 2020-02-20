444384960558ffea541a2318533b6d8b939e283c
gustavo <gustavo.maia@b2b.com.pt>
2020-02-20 17:26:02 +0000

    Classe OpcaoCliente

---
+++ app/src/main/java/com/vvm/sh/ui/agenda/OpcaoCliente.java
@@ -0,0 +1,10 @@
+package com.vvm.sh.ui.agenda;
+
+import com.vvm.sh.util.adaptadores.Item;
+
+public class OpcaoCliente extends Item {
+
+    public OpcaoCliente(int id, String descricao) {
+        super(id, descricao);
+    }
+}

ab5699eeb55b522f2ba858e5830c3504d492aa83
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-02-23 14:28:58 +0000

    Layout activity_anomalias

---
+++ app/src/main/java/com/vvm/sh/ui/anomalias/AnomaliasActivity.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.ui.anomalias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class AnomaliasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anomalias);
+    }
+}

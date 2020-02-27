f6f26b461f379f8f49b375fed9f67a7ef36bd42e
gustavo <gustavo.maia@b2b.com.pt>
2020-02-27 16:06:31 +0000

    Integração da AcaoFormacaoActivity com a FormacaoActivity

--- app/src/main/java/com/vvm/sh/ui/atividadesPendentes/FormacaoActivity.java
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/FormacaoActivity.java
@@ -2,9 +2,13 @@ package com.vvm.sh.ui.atividadesPendentes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.vvm.sh.R;
+
+import butterknife.OnClick;
 
 public class FormacaoActivity extends AppCompatActivity {
 
@@ -13,4 +17,52 @@ public class FormacaoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formacao);
     }
+
+
+
+    /**
+     * Metodo que permite iniciar o formulario de formando
+     */
+    public void initFormando(){
+
+        /*
+        Intent intent = new Intent(this, IndiceFormando_Activity.class);
+
+        Bundle bundle = new Bundle();
+        if(adaptador.obterRegistoSelecionado() != null) {
+
+            bundle.putString(BundleIF.ID_RELATORIO, adaptador.obterRegistoSelecionado().obterId());
+        }
+        else{
+            bundle.putString(BundleIF.ID_RELATORIO, AppIF.SEM_DADOS);
+        }
+
+        bundle.putString(BundleIF.ID_ATIVIDADE_PENDENTE, idAtividadePendente);
+        intent.putExtras(bundle);
+        startActivityForResult(intent, CodigoAtividadeIF.REGISTO_FORMANDO);
+        */
+    }
+
+
+
+    //-------------------
+    //Eventos
+    //-------------------
+
+
+    @OnClick(R.id.fab_adicionar_formando)
+    public void fab_adicionar_formando_OnClickListener(View view) {
+        initFormando();
+    }
+
+
+
+    @OnClick(R.id.fab_adicionar_acao_formacao)
+    public void fab_adicionar_acao_formacao_OnClickListener(View view) {
+
+        Intent intent = new Intent(this, AcaoFormacaoActivity.class);
+        //intent.putExtra(AppConstants.PICTURE, pictureRecyclerAdapter.getSelectedPicture(position).getId());
+        startActivityForResult(intent, 222222);
+    }
+
 }

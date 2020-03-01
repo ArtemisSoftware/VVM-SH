eab800ec74e0dc71e720ff2ece4dddbcdef32c60
ArtemisSoftware <gustavo.isaac@gmail.com>
2020-03-01 18:20:20 +0000

    FormandoRecyclerAdapter

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/FormandoRecyclerAdapter.java
@@ -0,0 +1,31 @@
+package com.vvm.sh.ui.atividadesPendentes;
+
+import android.view.View;
+
+import com.vvm.sh.R;
+import com.vvm.sh.util.adaptadores.ItemRecyclerAdapter;
+import com.vvm.sh.util.adaptadores.ItemViewHolder;
+import com.vvm.sh.util.interfaces.OnCheckBoxItemListener;
+import com.vvm.sh.util.interfaces.OnItemListener;
+
+public class FormandoRecyclerAdapter extends ItemRecyclerAdapter {
+
+    private OnItemListener onItemListener;
+    private OnCheckBoxItemListener onCheckBoxItemListener;
+
+
+    public FormandoRecyclerAdapter(OnItemListener onItemListener, OnCheckBoxItemListener onCheckBoxItemListener) {
+        this.onItemListener = onItemListener;
+        this.onCheckBoxItemListener = onCheckBoxItemListener;
+    }
+
+    @Override
+    protected int obterLayout(int viewType) {
+        return R.layout.formando_list_card_iterm;
+    }
+
+    @Override
+    protected ItemViewHolder obterViewHolder(View view, int viewType) {
+        return new FormandoViewHolder(view, this.onItemListener, this.onCheckBoxItemListener);
+    }
+}

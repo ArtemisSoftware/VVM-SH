7c49453f02b2644add79e0cdcf80bf4283b28836
gustavo <gustavo.maia@b2b.com.pt>
2020-03-02 09:09:06 +0000

    Layout dialogo_atividade_pendente_executada

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/DialogoAtividadePendenteExecutada.java
@@ -0,0 +1,20 @@
+package com.vvm.sh.ui.atividadesPendentes;
+
+import android.app.AlertDialog;
+
+import com.vvm.sh.R;
+import com.vvm.sh.ui.BaseDialogFragment;
+
+public class DialogoAtividadePendenteExecutada extends BaseDialogFragment {
+
+
+    @Override
+    protected void criarDialogo(AlertDialog.Builder builder) {
+
+    }
+
+    @Override
+    protected int obterLayout() {
+        return R.layout.dialogo_atividade_pendente_executada;
+    }
+}

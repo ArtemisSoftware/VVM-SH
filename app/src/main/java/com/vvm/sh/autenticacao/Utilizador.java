8589a592d91be9d301f7eb4a6b6ad0c2fc53d538
gustavo <gustavo.maia@b2b.com.pt>
2020-03-04 10:41:37 +0000

    Classe utilizador

---
+++ app/src/main/java/com/vvm/sh/autenticacao/Utilizador.java
@@ -0,0 +1,43 @@
+package com.vvm.sh.autenticacao;
+
+public class Utilizador {
+
+    private String id, nome, cap;
+
+
+    public Utilizador (String id, String nome, String cap){
+        this.id = id;
+        this.nome = nome;
+        this.cap = cap;
+    }
+
+
+
+    /**
+     * Metodo que devolve o identificador do utilizador
+     * @return o identificador
+     */
+    public String obterId(){
+        return id;
+    }
+
+
+    /**
+     * Metodo que devolve o nome do utilizador
+     * @return o nome
+     */
+    public String obterNome(){
+        return nome;
+    }
+
+
+    /**
+     * Metodo que devolve o cap do utilizador
+     * @return o cap
+     */
+    public String obterCap(){
+        return cap;
+    }
+
+
+}

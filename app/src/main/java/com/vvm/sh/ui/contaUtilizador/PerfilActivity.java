d9f78bb749e5dfdfe5c4495bf524bdf8fb874744
gustavo <gustavo.maia@b2b.com.pt>
2020-03-04 10:36:17 +0000

    PerfilActivity

--- app/src/main/java/com/vvm/sh/ui/contaUtilizador/PerfilActivity.java
+++ app/src/main/java/com/vvm/sh/ui/contaUtilizador/PerfilActivity.java
@@ -2,25 +2,81 @@ package com.vvm.sh.ui.contaUtilizador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.amulyakhare.textdrawable.TextDrawable;
 import com.vvm.sh.R;
+import com.vvm.sh.ui.BaseActivity;
+import com.vvm.sh.util.adaptadores.Item;
 
-public class PerfilActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+public class PerfilActivity extends BaseActivity {
+
+    @BindView(R.id.img_perfil)
+    ImageView img_perfil;
+
+
+    @BindView(R.id.txt_nome)
+    TextView txt_nome;
+
+    @BindView(R.id.txt_numero)
+    TextView txt_numero;
+
+    @BindView(R.id.txt_cap)
+    TextView txt_cap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+
+        subscreverObservadores();
+        obterRegistos();
     }
 
-    /*
 
-    TextDrawable drawable = TextDrawable.builder()
-                .buildRect("A", Color.RED);
+    //------------------------
+    //Metodos locais
+    //------------------------
 
-ImageView image = (ImageView) findViewById(R.id.image_view);
-image.setImageDrawable(drawable);
 
+    private void obterRegistos(){
+
+        //--TESTE (apagar quando houver dados)
+
+        TextDrawable drawable = TextDrawable.builder().buildRect("LI", Color.GREEN);
+
+        img_perfil.setImageDrawable(drawable);
+
+
+        /*
+        List<Item> t1 = new ArrayList<>();
+        t1.add(new Tarefa(1, "Tarefa numero 1", "SH"));
+        t1.add(new Tarefa(2, "Tarefa numero 2", "SA"));
+
+        tarefaRecyclerAdapter.fixarRegistos(t1);
+*/
+        //TODO: chamar metodo do viewmodel
+    }
+
+    /**
+     * Metodo que permite subscrever observadores
      */
+    private void subscreverObservadores(){
+
+
+        //TODO: subscrever observadores do viewmodel
+
+    }
+
+
 }

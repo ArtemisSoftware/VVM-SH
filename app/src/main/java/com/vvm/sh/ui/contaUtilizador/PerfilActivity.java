083be21214ba970488beb00c95e6ccd3b30e1c4a
gustavo <gustavo.maia@b2b.com.pt>
2020-03-04 10:19:41 +0000

    Layout activity_perfil

---
+++ app/src/main/java/com/vvm/sh/ui/contaUtilizador/PerfilActivity.java
@@ -0,0 +1,26 @@
+package com.vvm.sh.ui.contaUtilizador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+
+    /*
+
+    TextDrawable drawable = TextDrawable.builder()
+                .buildRect("A", Color.RED);
+
+ImageView image = (ImageView) findViewById(R.id.image_view);
+image.setImageDrawable(drawable);
+
+     */
+}

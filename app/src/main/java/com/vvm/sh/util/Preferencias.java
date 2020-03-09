d85ba88855a3128bdcea3b38fb541d48c8ff1862
gustavo <gustavo.maia@b2b.com.pt>
2020-03-09 10:06:34 +0000

    Atualização da classe Preferencias

--- app/src/main/java/com/vvm/sh/util/Preferencias.java
+++ app/src/main/java/com/vvm/sh/util/Preferencias.java
@@ -10,6 +10,8 @@ public class Preferencias {
 
     private static final String PRIMEIRA_UTILIZACAO = "primeira_utilizacao";
     private static final String VERSAO_APP = "versao_app";
+    private static final String ID_UTILIZADOR = "id_utilizador";
+    private static final String TOKEN = "token";
 
 
     /**
@@ -30,6 +32,41 @@ public class Preferencias {
 
 
     /**
+     * Metodo que permite fixar os dados do utilizador
+     * @param contexto
+     * @param idUtilizador o identificador do utilizador
+     * @param token o token associado
+     */
+    public static void fixarDadosUtilizador(Context contexto, String idUtilizador, String token){
+
+        String pacote = contexto.getPackageName();
+
+        SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE); // 0 - for private mode
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.putString(ID_UTILIZADOR, idUtilizador);
+        editor.putString(TOKEN, token);
+        editor.commit();
+    }
+
+
+    /**
+     * Metodo que permite atualizar a versao da aplicacao
+     * @param contexto
+     */
+    public static void atualizarVersaoApp(Context contexto){
+
+        String pacote = contexto.getPackageName();
+
+        SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE); // 0 - for private mode
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.putString(VERSAO_APP, BuildConfig.VERSION_NAME);
+        editor.commit();
+    }
+
+
+
+
+    /**
      * Metodo que permite obter o estado da primeira utilizacao
      * @param contexto
      * @return true caso seja a primeira utilizacao ou false caso contrario
@@ -41,7 +78,6 @@ public class Preferencias {
         SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE);
         return preferencias.getBoolean(PRIMEIRA_UTILIZACAO, true);
     }
-
 
 
     /**
@@ -57,6 +93,53 @@ public class Preferencias {
         String versao = preferencias.getString(VERSAO_APP, "0.0.0");
 
         return !(BuildConfig.VERSION_NAME.equals(versao));
+    }
+
+
+    /**
+     * Metodo que permite obter o identificador do utilizador
+     * @param contexto
+     * @return o identificador do utilizador
+     */
+    public static String obterIdUtilizador(Context contexto) {
+
+        String pacote = contexto.getPackageName();
+
+        SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE);
+        return preferencias.getString(ID_UTILIZADOR, "");
+    }
+
+
+    /**
+     * Metodo que permite obter o token do utilizador
+     * @param contexto
+     * @return o token do utilizador
+     */
+    public static String obterToken(Context contexto) {
+
+        String pacote = contexto.getPackageName();
+
+        SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE);
+        return preferencias.getString(TOKEN, "");
+    }
+
+
+
+
+    /**
+     * Metodo que permite eliminar os dados do utilizador
+     * @param contexto
+     */
+    public static void eliminarDadosUtilizador(Context contexto){
+
+        String pacote = contexto.getPackageName();
+
+        SharedPreferences preferencias = contexto.getSharedPreferences(pacote, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencias.edit();
+
+        editor.remove(ID_UTILIZADOR);
+        editor.remove(TOKEN);
+        editor.apply();
     }
 
 

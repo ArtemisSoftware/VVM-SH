049e4a0c033e045b41e4c155e0a5485673fbb232
gustavo <gustavo.maia@b2b.com.pt>
2020-07-14 17:13:15 +0100

    AppComponent

---
+++ app/src/main/java/com/vvm/sh/di/AppComponent.java
@@ -0,0 +1,38 @@
+package com.vvm.sh.di;
+
+import android.app.Application;
+
+import com.vvm.sh.App;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjectionModule;
+import dagger.android.AndroidInjector;
+
+
+@Singleton
+@Component(
+        modules = {
+                AndroidInjectionModule.class,
+                ActivityBuildersModule.class,
+                //AppModule.class,
+                ViewModelFactoryModule.class,
+
+                //DatabaseModule.class,
+                //NetworkModule.class,
+
+        }
+)
+public interface AppComponent extends AndroidInjector<App> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}

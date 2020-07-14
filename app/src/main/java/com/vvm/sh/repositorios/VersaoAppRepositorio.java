ef2d7d2cb776f0acf487ffcb1e009d6b758fe196
gustavo <gustavo.maia@b2b.com.pt>
2020-07-14 17:40:15 +0100

    VersaoAppRepositorio

---
+++ app/src/main/java/com/vvm/sh/repositorios/VersaoAppRepositorio.java
@@ -0,0 +1,27 @@
+package com.vvm.sh.repositorios;
+
+import androidx.annotation.NonNull;
+
+import com.vvm.sh.api.SegurancaAlimentarApi;
+import com.vvm.sh.api.modelos.VersaoApp;
+
+import io.reactivex.Single;
+
+public class VersaoAppRepositorio {
+
+
+    private final SegurancaAlimentarApi api;
+
+
+    public VersaoAppRepositorio(@NonNull SegurancaAlimentarApi api) {
+        this.api = api;
+    }
+
+
+
+    public Single<VersaoApp> obterAtualizacao() {
+        return api.obterAtualizacao();
+    }
+
+
+}

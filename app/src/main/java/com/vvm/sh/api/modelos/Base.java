8af4e26be4e6af2aa3e6673bc055b5cfb76f0966
gustavo <gustavo.maia@b2b.com.pt>
2020-07-14 15:50:56 +0100

    Modelo base para a api

---
+++ app/src/main/java/com/vvm/sh/api/modelos/Base.java
@@ -0,0 +1,9 @@
+package com.vvm.sh.api.modelos;
+
+import com.google.gson.annotations.SerializedName;
+
+public abstract class Base {
+
+    @SerializedName("TimeStamp")
+    public String timeStamp;
+}

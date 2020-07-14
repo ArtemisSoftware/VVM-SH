07d7045132898f7130bf904705e9a6dd8e8f576d
gustavo <gustavo.maia@b2b.com.pt>
2020-07-14 17:12:07 +0100

    ActivityBuildersModule

---
+++ app/src/main/java/com/vvm/sh/di/ActivityBuildersModule.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.di;
+
+import dagger.Module;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    /*
+    @PokemonScope
+    @ContributesAndroidInjector(
+            modules = { PokemonViewModelsModule.class, PokemonModule.class }
+    )
+    abstract FavouritesActivity contributeFavouritesActivity();
+*/
+}

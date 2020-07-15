bd5c76ecd3a7bba5875701ecca56a90040ff960b
gustavo <gustavo.maia@b2b.com.pt>
2020-07-15 18:47:58 +0100

    TipoRecyclerAdapter

---
+++ app/src/main/java/com/vvm/sh/ui/opcoes/adaptadores/TipoRecyclerAdapter.java
@@ -0,0 +1,66 @@
+package com.vvm.sh.ui.opcoes.adaptadores;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vvm.sh.R;
+import com.vvm.sh.databinding.ItemTipoBinding;
+import com.vvm.sh.ui.contaUtilizador.Colecao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TipoRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+
+    private List<Colecao> items = new ArrayList<>();
+    private Context contexto;
+    //private NotesFragment.OnNoteLongPressListener onItemLongListener;
+
+    public TipoRecyclerAdapter(Context contexto, List<Colecao> items/*, NotesFragment.OnNoteLongPressListener onItemLongListener*/) {
+        this.items = items;
+        this.contexto = contexto;
+        //this.onItemLongListener = onItemLongListener;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        ItemTipoBinding binding = DataBindingUtil.inflate(LayoutInflater.from(contexto), R.layout.item_tipo, parent, false);
+        return new TipoViewHolder(binding.getRoot()/*, this.onItemLongListener*/);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        Colecao register = items.get(position);
+//        ((TipoViewHolder)holder).binding.setNote(register);
+//        ((TipoViewHolder)holder).binding.setListener((OnPokemonListener) context);
+
+        ((TipoViewHolder)holder).binding.executePendingBindings();
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+
+    public void atualizar(List<Colecao> items){
+        this.items.clear();
+        this.items.addAll(items);
+        notifyDataSetChanged();
+    }
+
+
+
+}

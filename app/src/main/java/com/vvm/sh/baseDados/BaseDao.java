8e0eb98c6c3a21e21dfab66508fb592718d104e9
gustavo <gustavo.maia@b2b.com.pt>
2020-07-16 11:19:55 +0100

    BaseDao

---
+++ app/src/main/java/com/vvm/sh/baseDados/BaseDao.java
@@ -0,0 +1,31 @@
+package com.vvm.sh.baseDados;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Update;
+
+import io.reactivex.Single;
+
+@Dao
+public interface BaseDao<T> {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    Single<Long> inserir(T entity);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void inserir(T... entity);
+
+
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    Single<Integer> atualizar(T entity);
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    void atualizar(T... entity);
+
+
+    @Delete
+    Single<Integer> remover(T entity);
+}

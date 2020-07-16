35edf2b74cc366f9bf4a56f45315e363f748f3f6
gustavo <gustavo.maia@b2b.com.pt>
2020-07-16 11:38:15 +0100

    BaseDadosModule

---
+++ app/src/main/java/com/vvm/sh/di/opcoes/BaseDadosModule.java
@@ -0,0 +1,28 @@
+package com.vvm.sh.di.opcoes;
+
+import android.app.Application;
+
+import androidx.room.Room;
+
+import com.vvm.sh.baseDados.BaseDadosContantes;
+import com.vvm.sh.baseDados.VvmshBaseDados;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class BaseDadosModule {
+
+    @Singleton
+    @Provides
+    static VvmshBaseDados provideVvmshBaseDados(Application application){
+        return Room.databaseBuilder(application, VvmshBaseDados.class, BaseDadosContantes.NOME)
+                //.addMigrations(MigrationDb.getMigrations())
+                .build();
+    }
+
+
+
+}

8f76362001a49cdfab47870f1cd5312ae6e89a10
gustavo <gustavo.maia@b2b.com.pt>
2020-07-20 12:58:54 +0100

    UtilizadorDao

---
+++ app/src/main/java/com/vvm/sh/baseDados/UtilizadorDao.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.baseDados;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.vvm.sh.autenticacao.Utilizador;
+
+import io.reactivex.Completable;
+
+@Dao
+abstract public class UtilizadorDao implements BaseDao<Utilizador>{
+
+    @Query("DELETE FROM utilizadores WHERE id = :idUtilizador")
+    abstract public Completable remover(String idUtilizador);
+
+}

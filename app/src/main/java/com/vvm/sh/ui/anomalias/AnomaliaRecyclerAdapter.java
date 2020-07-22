bfaaacf5b4af1686b03bd90bbebc8acd9c422bd8
gustavo <gustavo.maia@b2b.com.pt>
2020-07-22 16:26:14 +0100

    Atualização AnomaliaViewHolder

--- app/src/main/java/com/vvm/sh/ui/anomalias/AnomaliaRecyclerAdapter.java
+++ app/src/main/java/com/vvm/sh/ui/anomalias/AnomaliaRecyclerAdapter.java
@@ -14,6 +14,25 @@ public class AnomaliaRecyclerAdapter extends ItemRecyclerAdapter {
 
     @Override
     protected ItemViewHolder obterViewHolder(View view, int viewType) {
-        return new AnomaliaViewHolder(view);
+        return null;//new AnomaliaViewHolder(view);
     }
+
+
+
+    /*
+    Anomalia registo = (Anomalia) item;
+
+        txt_observacao.setText(registo.obterObservacao());
+
+        if(registo.obterTipoAnomalia() == Anomalia.TIPO_ANOMALIA) {
+            txt_data.setText(registo.obterData());
+            txt_contacto.setText(registo.obterContacto());
+            txt_tipo.setText(registo.obterTipo());
+        }
+        else{
+            lnr_lyt_data.setVisibility(View.GONE);
+            lnr_lyt_contacto.setVisibility(View.GONE);
+            lnr_lyt_tipo.setVisibility(View.GONE);
+        }
+    */
 }
--- app/src/main/java/com/vvm/sh/ui/anomalias/AnomaliaViewHolder.java
+++ app/src/main/java/com/vvm/sh/ui/anomalias/AnomaliaViewHolder.java
@@ -6,59 +6,24 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.vvm.sh.R;
+import com.vvm.sh.databinding.ItemAnomaliaBinding;
 import com.vvm.sh.util.adaptadores.Item;
 import com.vvm.sh.util.adaptadores.ItemViewHolder;
 
 import butterknife.BindView;
 
-public class AnomaliaViewHolder extends ItemViewHolder {
+public class AnomaliaViewHolder extends RecyclerView.ViewHolder {
 
-    @BindView(R.id.lnr_lyt_data)
-    LinearLayout lnr_lyt_data;
-
-    @BindView(R.id.txt_data)
-    TextView txt_data;
-
-
-    @BindView(R.id.lnr_lyt_contacto)
-    LinearLayout lnr_lyt_contacto;
-
-    @BindView(R.id.txt_contacto)
-    TextView txt_contacto;
-
-
-    @BindView(R.id.lnr_lyt_tipo)
-    LinearLayout lnr_lyt_tipo;
-
-    @BindView(R.id.txt_tipo)
-    TextView txt_tipo;
-
-    @BindView(R.id.txt_observacao)
-    TextView txt_observacao;
+    ItemAnomaliaBinding binding;
 
     public AnomaliaViewHolder(@NonNull View itemView) {
         super(itemView);
+
+        binding = DataBindingUtil.bind(itemView);
     }
 
-    @Override
-    protected void preencherCampos(Item item) {
-
-        Anomalia registo = (Anomalia) item;
-
-        txt_observacao.setText(registo.obterObservacao());
-
-        if(registo.obterTipoAnomalia() == Anomalia.TIPO_ANOMALIA) {
-            txt_data.setText(registo.obterData());
-            txt_contacto.setText(registo.obterContacto());
-            txt_tipo.setText(registo.obterTipo());
-        }
-        else{
-            lnr_lyt_data.setVisibility(View.GONE);
-            lnr_lyt_contacto.setVisibility(View.GONE);
-            lnr_lyt_tipo.setVisibility(View.GONE);
-        }
-
-    }
 }

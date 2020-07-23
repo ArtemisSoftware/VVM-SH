659ab74935478e051cab2811881ff25105569c9d
gustavo <gustavo.maia@b2b.com.pt>
2020-07-23 19:24:50 +0100

    OcorrenciasModule

---
+++ app/src/main/java/com/vvm/sh/di/ocorrencias/OcorrenciasModule.java
@@ -0,0 +1,34 @@
+package com.vvm.sh.di.ocorrencias;
+
+import com.vvm.sh.baseDados.AtividadePendenteDao;
+import com.vvm.sh.baseDados.OcorrenciaDao;
+import com.vvm.sh.baseDados.VvmshBaseDados;
+import com.vvm.sh.repositorios.OcorrenciaRepositorio;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class OcorrenciasModule {
+
+    @OcorrenciasScope
+    @Provides
+    static OcorrenciaDao provideOcorrenciaDao(VvmshBaseDados vvmshBaseDados){
+
+        OcorrenciaDao dao = vvmshBaseDados.obterOcorrenciaDao();
+
+        //Timber.d("Providing NoteDao: " + dao);
+        return dao;
+    }
+
+
+    @OcorrenciasScope
+    @Provides
+    OcorrenciaRepositorio provideOcorrenciaRepositorio(OcorrenciaDao ocorrenciaDao) {
+
+        OcorrenciaRepositorio repositorio = new OcorrenciaRepositorio(ocorrenciaDao);
+
+        //Timber.d("Providing PokemonRepository: " + repository);
+        return repositorio;
+    }
+}

7be416bb63308a10a2e25cdac64828348b5d2a71
gustavo <gustavo.maia@b2b.com.pt>
2020-07-23 19:35:14 +0100

    AtividadesPendentesViewModelsModule

---
+++ app/src/main/java/com/vvm/sh/di/atividadesPendentes/AtividadesPendentesViewModelsModule.java
@@ -0,0 +1,19 @@
+package com.vvm.sh.di.atividadesPendentes;
+
+import androidx.lifecycle.ViewModel;
+
+import com.vvm.sh.di.ViewModelKey;
+import com.vvm.sh.ui.atividadesPendentes.AtividadesPendentesViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class AtividadesPendentesViewModelsModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AtividadesPendentesViewModel.class)
+    public abstract ViewModel bindAtividadesPendentesViewModel(AtividadesPendentesViewModel viewModel);
+}

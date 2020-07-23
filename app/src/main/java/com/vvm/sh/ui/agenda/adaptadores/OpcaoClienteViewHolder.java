1d7d1c5d76c82ee5674d1ebc9c7a9b61a6d238e4
gustavo <gustavo.maia@b2b.com.pt>
2020-07-23 21:19:07 +0100

    Atualização OpcaoClienteViewHolder

--- app/src/main/java/com/vvm/sh/ui/agenda/adaptadores/OpcaoClienteViewHolder.java
+++ app/src/main/java/com/vvm/sh/ui/agenda/adaptadores/OpcaoClienteViewHolder.java
@@ -3,26 +3,28 @@ package com.vvm.sh.ui.agenda.adaptadores;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.vvm.sh.databinding.ItemOpcaoClienteBinding;
 import com.vvm.sh.util.adaptadores.Item;
 import com.vvm.sh.util.adaptadores.ItemViewHolder;
 import com.vvm.sh.util.interfaces.OnItemListener;
 
-public class OpcaoClienteViewHolder extends ItemViewHolder implements View.OnClickListener {
+public class OpcaoClienteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+    ItemOpcaoClienteBinding binding;
     private OnItemListener onItemListener;
 
     public OpcaoClienteViewHolder(@NonNull View itemView, OnItemListener onItemListener) {
         super(itemView);
+        binding = DataBindingUtil.bind(itemView);
 
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
     }
 
-    @Override
-    protected void preencherCampos(Item item) {
 
-    }
 
     @Override
     public void onClick(View v) {

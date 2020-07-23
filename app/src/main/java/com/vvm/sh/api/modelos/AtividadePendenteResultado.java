3202e4b75bbc63e04b483c03befed5e00e66d012
gustavo <gustavo.maia@b2b.com.pt>
2020-07-23 09:17:09 +0100

    AtividadePendenteResultado

---
+++ app/src/main/java/com/vvm/sh/api/modelos/AtividadePendenteResultado.java
@@ -0,0 +1,23 @@
+package com.vvm.sh.api.modelos;
+
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Classe que representa uma atividade pendente proveniente do web service
+ */
+public class AtividadePendenteResultado {
+
+
+    @SerializedName("ServID")
+    public String servID;
+
+    @SerializedName("Descricao")
+    public String descricao;
+
+    @SerializedName("DataProg")
+    public String dataProgramada;
+
+    @SerializedName("formacao")
+    public boolean formacao;
+}

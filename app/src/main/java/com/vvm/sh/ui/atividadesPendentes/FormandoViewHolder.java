f83827db23c5d555a280cef1bb1cccba9ab122f3
gustavo <gustavo.maia@b2b.com.pt>
2020-07-30 11:29:04 +0100

    Atualização do FormandoViewHolder

--- app/src/main/java/com/vvm/sh/ui/atividadesPendentes/FormandoViewHolder.java
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/FormandoViewHolder.java
@@ -6,9 +6,13 @@ import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.vvm.sh.R;
+import com.vvm.sh.databinding.ItemFormandoBinding;
 import com.vvm.sh.ui.atividadesPendentes.relatorios.Formando;
+import com.vvm.sh.ui.atividadesPendentes.relatorios.OnFormacaoListener;
 import com.vvm.sh.util.adaptadores.Item;
 import com.vvm.sh.util.adaptadores.ItemViewHolder;
 import com.vvm.sh.util.interfaces.OnCheckBoxItemListener;
@@ -16,50 +20,33 @@ import com.vvm.sh.util.interfaces.OnItemListener;
 
 import butterknife.BindView;
 
-public class FormandoViewHolder extends ItemViewHolder implements View.OnClickListener, CheckBox.OnCheckedChangeListener {
+public class FormandoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, CheckBox.OnCheckedChangeListener {
 
 
-    @BindView(R.id.chk_selecionado)
-    CheckBox chk_selecionado;
+    public ItemFormandoBinding binding;
+
+    private OnFormacaoListener listener;
 
 
-    //@BindView(R.id.txt_identificacao)
-    TextView txt_identificacao;
-
-
-    private OnItemListener onItemListener;
-    private OnCheckBoxItemListener onCheckBoxItemListener;
-
-
-    public FormandoViewHolder(@NonNull View itemView, OnItemListener onItemListener, OnCheckBoxItemListener onCheckBoxItemListener) {
+    public FormandoViewHolder(@NonNull View itemView, OnFormacaoListener listener) {
         super(itemView);
 
-        this.onItemListener = onItemListener;
+        binding = DataBindingUtil.bind(itemView);
+
+        this.listener = listener;
         itemView.setOnClickListener(this);
-        this.onCheckBoxItemListener = onCheckBoxItemListener;
-    }
-
-
-    @Override
-    protected void preencherCampos(Item item) {
-
-//        Formando registo = (Formando) item;
-//
-//        chk_selecionado.setChecked(registo.obterSelecao());
-//        txt_identificacao.setText(registo.obterBiCartaoCidadao());
-
-        chk_selecionado.setOnCheckedChangeListener(this);
+        binding.chkSelecionado.setOnCheckedChangeListener(this);
     }
 
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        onCheckBoxItemListener.onItemChecked(getAdapterPosition(), isChecked);
+        this.listener.OnSelecionadoCheck(binding.getFormando(), isChecked);
     }
 
 
     @Override
     public void onClick(View v) {
-        onItemListener.onItemClick(getAdapterPosition());
+        this.listener.OnFormandoClick(binding.getFormando());
     }
 }
--- app/src/main/java/com/vvm/sh/ui/atividadesPendentes/relatorios/OnFormacaoListener.java
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/relatorios/OnFormacaoListener.java
@@ -3,4 +3,6 @@ package com.vvm.sh.ui.atividadesPendentes.relatorios;
 public interface OnFormacaoListener {
 
     void OnFormandoClick(Formando formando);
+
+    void OnSelecionadoCheck(Formando formando, boolean selecionado);
 }

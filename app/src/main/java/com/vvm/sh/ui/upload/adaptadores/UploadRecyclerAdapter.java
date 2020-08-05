feed424ed972a4b8ce48816116f16d4a66a4f82b
gustavo <gustavo.maia@b2b.com.pt>
2020-08-05 18:38:21 +0100

    UploadRecyclerAdapter

---
+++ app/src/main/java/com/vvm/sh/ui/upload/adaptadores/UploadRecyclerAdapter.java
@@ -0,0 +1,63 @@
+package com.vvm.sh.ui.upload.adaptadores;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vvm.sh.R;
+import com.vvm.sh.databinding.ItemOcorrenciaBinding;
+import com.vvm.sh.baseDados.entidades.Ocorrencia;
+import com.vvm.sh.ui.upload.modelos.Upload;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UploadRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+
+    private List<Upload> items = new ArrayList<>();
+    private Context contexto;
+
+    public UploadRecyclerAdapter(Context contexto, List<Upload> items) {
+        this.items = items;
+        this.contexto = contexto;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        ItemOcorrenciaBinding binding = DataBindingUtil.inflate(LayoutInflater.from(contexto), R.layout.item_ocorrencia, parent, false);
+        return new UploadViewHolder(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        Upload registo = items.get(position);
+        ((UploadViewHolder)holder).binding.setUpload(registo);
+
+        ((UploadViewHolder)holder).binding.executePendingBindings();
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+
+    public void atualizar(List<Upload> items){
+        this.items.clear();
+        this.items.addAll(items);
+        notifyDataSetChanged();
+    }
+
+
+}
+

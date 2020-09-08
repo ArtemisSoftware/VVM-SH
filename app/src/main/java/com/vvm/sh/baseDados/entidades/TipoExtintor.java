dcd0c25f594a8074a1ddff4a57834c4ee96c91bb
gustavo <gustavo.maia@b2b.com.pt>
2020-09-08 12:39:19 +0100

    Entidade TipoExtintor

---
+++ app/src/main/java/com/vvm/sh/baseDados/entidades/TipoExtintor.java
@@ -0,0 +1,39 @@
+package com.vvm.sh.baseDados.entidades;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(tableName = "tiposExtintor",
+        indices = {@Index(value="descricao", unique = false) },
+        primaryKeys = {"idTarefa","id"},
+        foreignKeys = @ForeignKey(entity = Tarefa.class,
+                parentColumns = "idTarefa",
+                childColumns = "idTarefa",
+                onDelete = CASCADE))
+public class TipoExtintor {
+
+
+    @NonNull
+    public int idTarefa;
+
+
+    @NonNull
+    public String id;
+
+
+    @NonNull
+    @ColumnInfo(name = "descricao")
+    public String descricao;
+
+
+    public TipoExtintor(int idTarefa, @NonNull String id, @NonNull String descricao) {
+        this.idTarefa = idTarefa;
+        this.id = id;
+        this.descricao = descricao;
+    }
+}

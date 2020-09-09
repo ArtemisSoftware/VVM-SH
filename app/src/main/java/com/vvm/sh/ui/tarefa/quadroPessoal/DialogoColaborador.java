5ea39fecb9a9e72fabbe3da72ba07b35fd464a94
gustavo <gustavo.maia@b2b.com.pt>
2020-09-09 14:35:23 +0100

    Layout dialogo_colaborador

---
+++ app/src/main/java/com/vvm/sh/ui/tarefa/quadroPessoal/DialogoColaborador.java
@@ -0,0 +1,34 @@
+package com.vvm.sh.ui.tarefa.quadroPessoal;
+
+import com.vvm.sh.ui.BaseDaggerDialogoPersistenteFragment;
+
+public class DialogoColaborador extends BaseDaggerDialogoPersistenteFragment {
+
+
+
+
+    @Override
+    protected void iniciarDialogo() {
+
+    }
+
+    @Override
+    protected int obterLayout() {
+        return 0;
+    }
+
+    @Override
+    protected int obterTitulo() {
+        return 0;
+    }
+
+    @Override
+    protected void subscreverObservadores() {
+
+    }
+
+    @Override
+    protected void clickPositivo() {
+
+    }
+}

14d13f6d4ad6f1463521a5f55da7c61d0a2b57b0
gustavo <gustavo.maia@b2b.com.pt>
2020-09-10 15:35:06 +0100

    Layout activity_avaliacao_geral

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/relatorios/avaliacaoAmbiental/AvaliacaoGeralActivity.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.ui.atividadesPendentes.relatorios.avaliacaoAmbiental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class AvaliacaoGeralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avaliacao_geral);
+    }
+}

9b488aba8f926bed8401b7aba92fc570d3a7fec7
gustavo <gustavo.maia@b2b.com.pt>
2020-09-11 18:48:08 +0100

    PesquisaModule

---
+++ app/src/main/java/com/vvm/sh/di/pesquisa/PesquisaModule.java
@@ -0,0 +1,53 @@
+package com.vvm.sh.di.pesquisa;
+
+import com.vvm.sh.api.SegurancaAlimentarApi;
+import com.vvm.sh.baseDados.VvmshBaseDados;
+import com.vvm.sh.baseDados.dao.AtualizacaoDao;
+import com.vvm.sh.baseDados.dao.TipoDao;
+import com.vvm.sh.di.opcoes.OpcoesScope;
+import com.vvm.sh.repositorios.TiposRepositorio;
+import com.vvm.sh.repositorios.VersaoAppRepositorio;
+
+import dagger.Module;
+import dagger.Provides;
+
+
+@Module
+public class PesquisaModule {
+
+
+    @PesquisaScope
+    @Provides
+    VersaoAppRepositorio provideVersaoAppRepositorio(SegurancaAlimentarApi segurancaAlimentarApi) {
+
+        VersaoAppRepositorio repositorio = new VersaoAppRepositorio(segurancaAlimentarApi);
+
+        //Timber.d("Providing PokemonRepository: " + repository);
+        return repositorio;
+    }
+
+
+
+    @PesquisaScope
+    @Provides
+    static AtualizacaoDao provideAtualizacaoDao(VvmshBaseDados vvmshBaseDados){
+
+        AtualizacaoDao dao = vvmshBaseDados.obterAtualizacaoDao();
+
+        //Timber.d("Providing NoteDao: " + dao);
+        return dao;
+    }
+
+
+
+
+    @PesquisaScope
+    @Provides
+    TiposRepositorio provideTiposRepositorio(SegurancaAlimentarApi segurancaAlimentarApi, AtualizacaoDao atualizacaoDao, TipoDao tipoDao) {
+
+        TiposRepositorio repositorio = new TiposRepositorio(segurancaAlimentarApi, atualizacaoDao, tipoDao);
+
+        //Timber.d("Providing PokemonRepository: " + repository);
+        return repositorio;
+    }
+}

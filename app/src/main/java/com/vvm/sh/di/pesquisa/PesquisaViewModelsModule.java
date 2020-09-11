73a685f14fca611c29356f91ac2da81d286338e1
gustavo <gustavo.maia@b2b.com.pt>
2020-09-11 19:16:07 +0100

    PesquisaViewModelsModule

---
+++ app/src/main/java/com/vvm/sh/di/pesquisa/PesquisaViewModelsModule.java
@@ -0,0 +1,20 @@
+package com.vvm.sh.di.pesquisa;
+
+import androidx.lifecycle.ViewModel;
+
+import com.vvm.sh.di.ViewModelKey;
+import com.vvm.sh.ui.pesquisa.PesquisaViewModel;
+import com.vvm.sh.ui.quadroPessoal.QuadroPessoalViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class PesquisaViewModelsModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(PesquisaViewModel.class)
+    public abstract ViewModel bindPesquisaViewModel(PesquisaViewModel viewModel);
+}

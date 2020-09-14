7dca7282b123790f787ef6c3bdfa57429cc73d82
gustavo <gustavo.maia@b2b.com.pt>
2020-09-14 18:33:41 +0100

    LevantamentosScope

---
+++ app/src/main/java/com/vvm/sh/di/atividadesPendentes/levantamentos/LevantamentosScope.java
@@ -0,0 +1,13 @@
+package com.vvm.sh.di.atividadesPendentes.levantamentos;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LevantamentosScope {
+}

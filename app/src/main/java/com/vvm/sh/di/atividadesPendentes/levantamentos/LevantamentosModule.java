4b8342695c7f7ef87cab8957d5aabd6006f1a2c8
gustavo <gustavo.maia@b2b.com.pt>
2020-09-14 18:35:16 +0100

    LevantamentosModule

---
+++ app/src/main/java/com/vvm/sh/di/atividadesPendentes/levantamentos/LevantamentosModule.java
@@ -0,0 +1,26 @@
+package com.vvm.sh.di.atividadesPendentes.levantamentos;
+
+import com.vvm.sh.baseDados.VvmshBaseDados;
+import com.vvm.sh.baseDados.dao.CategoriaProfissionalDao;
+import com.vvm.sh.di.atividadesPendentes.avaliacaoAmbiental.AvaliacaoAmbientalScope;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class LevantamentosModule {
+
+    @LevantamentosScope
+    @Provides
+    static CategoriaProfissionalDao provideCategoriaProfissionalDao(VvmshBaseDados vvmshBaseDados){
+
+        CategoriaProfissionalDao dao = vvmshBaseDados.obterCategoriaProfissionalDao();
+
+        //Timber.d("Providing NoteDao: " + dao);
+        return dao;
+    }
+
+
+
+
+}

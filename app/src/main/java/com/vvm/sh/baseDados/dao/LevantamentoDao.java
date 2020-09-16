4a3c43d2ed6a2d6b6f314dfdf8adbd42c5e5501e
gustavo <gustavo.maia@b2b.com.pt>
2020-09-16 09:39:16 +0100

    Layout item_risco

--- app/src/main/java/com/vvm/sh/baseDados/dao/LevantamentoDao.java
+++ app/src/main/java/com/vvm/sh/baseDados/dao/LevantamentoDao.java
@@ -23,6 +23,10 @@ abstract public class LevantamentoDao implements BaseDao<LevantamentoRiscoResult
     abstract public Maybe<LevantamentoRiscoResultado> obterLevantamento(int id);
 
 
+
+//------validacao
+
+
 //    String query = " SELECT  ";
 //
 //    query += "CASE WHEN tarefa IS NULL OR perigo IS NULL THEN 0 ";
@@ -44,6 +48,25 @@ abstract public class LevantamentoDao implements BaseDao<LevantamentoRiscoResult
 //    String argumentos [] = {
 //            IdentificadoresIF.ORIGEM_LEVANTAMENTO_RISCO + "", idLevantamento
 //    };
+
+
+//    String query = "SELECT CASE WHEN COUNT(VALIDO) = SUM(VALIDO) AND COUNT(VALIDO) > 0 THEN 1 ELSE 0 END valido   ";
+//    query += "FROM (			  ";
+//    query += "SELECT idLevantamento,			  ";
+//    query += "CASE WHEN nd IS NULL THEN 0  			  ";
+//    query += "WHEN numeroMedidasExistentes IS NULL AND numeroMedidasRecomendadas IS NULL THEN 0  			  ";
+//    query += "WHEN numeroMedidasExistentes = 0 OR numeroMedidasRecomendadas = 0 THEN 0 			  ";
+//    query += "ELSE 1 END as valido			  ";
+//    query += "FROM riscos_resultado  as rsc_res			  ";
+//    query += "OUTER LEFT JOIN (SELECT id, COUNT(idMedida) as numeroMedidasExistentes  FROM medidas_resultado WHERE origem = ? GROUP BY id) as med_existentes ON  rsc_res.idRegisto = med_existentes.id			  ";
+//    query += "OUTER LEFT JOIN (SELECT id, COUNT(idMedida) as numeroMedidasRecomendadas  FROM medidas_resultado WHERE origem = ? GROUP BY id) as med_recomendadas ON  rsc_res.idRegisto = med_recomendadas.id			  ";
+//    query += ") as resultado			  ";
+//    query += "WHERE idLevantamento = ?		  ";
+//
+//    String argumentos [] = {
+//            IdentificadoresIF.ORIGEM_RISCO_MEDIDAS_EXISTENTES + "", IdentificadoresIF.ORIGEM_RISCO_MEDIDAS_RECOMENDADAS + "", idRelatorio
+//    };
+
 
 
 }

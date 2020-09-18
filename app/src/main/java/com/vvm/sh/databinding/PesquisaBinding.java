11a54b381861954531dd9164eb49b60846e1c200
gustavo <gustavo.maia@b2b.com.pt>
2020-09-18 13:02:03 +0100

    PesquisaBinding

--- app/src/main/java/com/vvm/sh/databinding/PesquisaBinding.java
+++ app/src/main/java/com/vvm/sh/databinding/PesquisaBinding.java
@@ -4,8 +4,10 @@ import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.vvm.sh.baseDados.entidades.Tipo;
 import com.vvm.sh.ui.pesquisa.OnPesquisaListener;
 import com.vvm.sh.ui.pesquisa.Pesquisa;
+import com.vvm.sh.ui.pesquisa.PesquisaRecyclerAdapter;
 
 import java.util.List;
 
@@ -13,7 +15,7 @@ public class PesquisaBinding {
 
 
     @BindingAdapter({"registosPesquisa", "listener"})
-    public static void setRegistosPesquisa(RecyclerView view, List<Pesquisa> items, OnPesquisaListener.OnPesquisaRegistoListener listener) {
+    public static void setRegistosPesquisa(RecyclerView view, List<Tipo> items, OnPesquisaListener.OnPesquisaRegistoListener listener) {
 
         if(items == null){
             return;
@@ -21,27 +23,27 @@ public class PesquisaBinding {
 
         RecyclerView.LayoutManager layoutManager = view.getLayoutManager();
 
-        /*
+
         if(layoutManager == null){
             view.setLayoutManager(new LinearLayoutManager(view.getContext()));
         }
 
-        ExtintorRecyclerAdapter adapter = (ExtintorRecyclerAdapter) view.getAdapter();
+        PesquisaRecyclerAdapter adapter = (PesquisaRecyclerAdapter) view.getAdapter();
 
         if(adapter == null){
-            adapter = new ExtintorRecyclerAdapter(view.getContext(), items, listener);
+            adapter = new PesquisaRecyclerAdapter(view.getContext(), items, listener);
 
             view.setAdapter(adapter);
         }
         else{
             adapter.atualizar(items);
         }
-        */
+
     }
 
 
     @BindingAdapter({"pesquisaSelecionado", "listener"})
-    public static void setsPesquisaSelecionado(RecyclerView view, List<Pesquisa> items, OnPesquisaListener.OnPesquisaSelecionadoListener listener) {
+    public static void setsPesquisaSelecionado(RecyclerView view, List<Tipo> items, OnPesquisaListener.OnPesquisaSelecionadoListener listener) {
 
         if(items == null){
             return;
@@ -49,22 +51,22 @@ public class PesquisaBinding {
 
         RecyclerView.LayoutManager layoutManager = view.getLayoutManager();
 
-        /*
+
         if(layoutManager == null){
             view.setLayoutManager(new LinearLayoutManager(view.getContext()));
         }
 
-        ExtintorRecyclerAdapter adapter = (ExtintorRecyclerAdapter) view.getAdapter();
+        PesquisaRecyclerAdapter adapter = (PesquisaRecyclerAdapter) view.getAdapter();
 
         if(adapter == null){
-            adapter = new ExtintorRecyclerAdapter(view.getContext(), items, listener);
+            adapter = new PesquisaRecyclerAdapter(view.getContext(), items, listener);
 
             view.setAdapter(adapter);
         }
         else{
             adapter.atualizar(items);
         }
-        */
+
     }
 
 

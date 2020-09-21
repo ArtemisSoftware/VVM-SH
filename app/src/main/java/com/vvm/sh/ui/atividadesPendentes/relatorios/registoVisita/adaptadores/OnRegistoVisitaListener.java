f1a5356300b2d25421b9b6dc40c031545e69a460
gustavo <gustavo.maia@b2b.com.pt>
2020-09-21 16:59:32 +0100

    OnRegistoVisitaListener

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/relatorios/registoVisita/adaptadores/OnRegistoVisitaListener.java
@@ -0,0 +1,7 @@
+package com.vvm.sh.ui.atividadesPendentes.relatorios.registoVisita.adaptadores;
+
+public interface OnRegistoVisitaListener {
+
+    void onItemChecked(int idTrabalhoRealizado, boolean selecao);
+
+}

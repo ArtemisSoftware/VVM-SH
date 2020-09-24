bffe54aa8ca8dc6d7c407eda8346324595fa15c6
gustavo <gustavo.maia@b2b.com.pt>
2020-09-24 17:18:38 +0100

    QuestionarioRecyclerAdapter

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/relatorios/checklist/adaptadores/QuestionarioRecyclerAdapter.java
@@ -0,0 +1,173 @@
+package com.vvm.sh.ui.atividadesPendentes.relatorios.checklist.adaptadores;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vvm.sh.R;
+import com.vvm.sh.databinding.ItemChecklistBinding;
+import com.vvm.sh.databinding.ItemChecklistObservacaoBinding;
+import com.vvm.sh.databinding.ItemChecklistPerguntaBinding;
+import com.vvm.sh.ui.atividadesPendentes.relatorios.checklist.modelos.Item;
+import com.vvm.sh.ui.atividadesPendentes.relatorios.checklist.modelos.Questao;
+import com.vvm.sh.util.constantes.Identificadores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionarioRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+
+    private List<Questao> items = new ArrayList<>();
+    private Context contexto;
+    private OnChecklistListener listener;
+
+    private final int QUESTAO = 1;
+    private final int OBSERVACAO = 2;
+    private final int UTS = 3;
+    private final int FOTOS = 4;
+
+
+    public QuestionarioRecyclerAdapter(Context contexto, List<Questao> items, OnChecklistListener listener) {
+        this.items = items;
+        this.contexto = contexto;
+        this.listener = listener;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        switch (viewType){
+
+            case QUESTAO:
+
+                ItemChecklistPerguntaBinding binding = DataBindingUtil.inflate(LayoutInflater.from(contexto), R.layout.item_checklist_pergunta, parent, false);
+                return new PerguntaViewHolder(binding.getRoot(), listener);
+
+
+
+            case OBSERVACAO:
+
+                ItemChecklistObservacaoBinding bindingObs = DataBindingUtil.inflate(LayoutInflater.from(contexto), R.layout.item_checklist_observacao, parent, false);
+                return new ObservacaoViewHolder(bindingObs.getRoot(), listener);
+
+
+
+            case FOTOS:
+
+                break;
+
+            case UTS:
+
+                break;
+            default:
+
+                break;
+
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public int getItemViewType(int position) {
+
+
+        int tipo = 0;
+        Questao questao = items.get(position);
+
+
+        switch (questao.registo.tipo){
+
+            case Identificadores.Checklist.TIPO_QUESTAO:
+
+                tipo = QUESTAO;
+                break;
+
+
+            case Identificadores.Checklist.TIPO_OBSERVACOES:
+
+                tipo = OBSERVACAO;
+                break;
+
+
+            case Identificadores.Checklist.TIPO_FOTOS:
+
+                tipo = FOTOS;
+                break;
+
+            case Identificadores.Checklist.TIPO_UTS:
+
+                tipo = UTS;
+                break;
+            default:
+
+                break;
+
+        }
+
+
+        return tipo;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        Questao registo = items.get(position);
+
+        switch (getItemViewType(position)){
+
+            case QUESTAO:
+
+                ((PerguntaViewHolder)holder).binding.setQuestao(registo);
+                ((PerguntaViewHolder)holder).binding.executePendingBindings();
+                break;
+
+
+            case OBSERVACAO:
+
+                ((ObservacaoViewHolder)holder).binding.setQuestao(registo);
+                ((ObservacaoViewHolder)holder).binding.executePendingBindings();
+                break;
+
+
+            case FOTOS:
+
+                break;
+
+            case UTS:
+
+                break;
+            default:
+
+                break;
+
+        }
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+
+    public void atualizar(List<Questao> items){
+        this.items.clear();
+        this.items.addAll(items);
+        notifyDataSetChanged();
+    }
+
+
+
+}

81bd776188db9bd236d4f565faadfe1970cbc139
gustavo <gustavo.maia@b2b.com.pt>
2020-09-26 19:19:26 +0100

    activity_trabalhador_vulneravel_registo

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/relatorios/trabalhadoresVulneraveis/TrabalhadorVulneravelRegistoActivity.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.ui.atividadesPendentes.relatorios.trabalhadoresVulneraveis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class TrabalhadorVulneravelRegistoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trabalhador_vulneravel_registo);
+    }
+}

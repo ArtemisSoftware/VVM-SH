265a5b225f4189f10c7fa5c64401fe03b0975785
gustavo <gustavo.maia@b2b.com.pt>
2020-09-28 10:09:40 +0100

    Levantamento

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/relatorios/levantamentos/modelos/Levantamento.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.ui.atividadesPendentes.relatorios.levantamentos.modelos;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Embedded;
+
+import com.vvm.sh.baseDados.entidades.LevantamentoRiscoResultado;
+
+public class Levantamento {
+
+    @Embedded
+    public LevantamentoRiscoResultado resultado;
+
+    @ColumnInfo(name = "modelo")
+    public String modelo;
+
+}

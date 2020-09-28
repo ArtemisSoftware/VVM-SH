0c98e5942407311d507aa6a3444ea2a5d54a4da2
gustavo <gustavo.maia@b2b.com.pt>
2020-09-28 11:10:52 +0100

    Proposta

---
+++ app/src/main/java/com/vvm/sh/ui/atividadesPendentes/relatorios/propostaPlanoAccao/modelos/Proposta.java
@@ -0,0 +1,21 @@
+package com.vvm.sh.ui.atividadesPendentes.relatorios.propostaPlanoAccao.modelos;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Embedded;
+
+import com.vvm.sh.baseDados.entidades.PropostaPlanoAcaoResultado;
+
+public class Proposta {
+
+    @Embedded
+    public PropostaPlanoAcaoResultado resultado;
+
+
+
+    @ColumnInfo(name = "descricao")
+    public String descricao;
+
+
+    @ColumnInfo(name = "selecionado")
+    public boolean selecionado;
+}

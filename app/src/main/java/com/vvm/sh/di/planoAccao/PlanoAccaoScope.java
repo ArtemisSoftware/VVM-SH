b0beabe37b84631562063eaeb5f2f7fe42ed3f51
gustavo <gustavo.maia@b2b.com.pt>
2020-09-29 11:08:04 +0100

    PlanoAccaoScope

---
+++ app/src/main/java/com/vvm/sh/di/planoAccao/PlanoAccaoScope.java
@@ -0,0 +1,13 @@
+package com.vvm.sh.di.planoAccao;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PlanoAccaoScope {
+}

57f2b3e5959da912eec8bcea4f769ad0c4b04de9
gustavo <gustavo.maia@b2b.com.pt>
2020-09-29 11:39:35 +0100

    TipoTemplateAvrLevantamento

---
+++ app/src/main/java/com/vvm/sh/baseDados/entidades/TipoTemplateAvrLevantamento.java
@@ -0,0 +1,33 @@
+package com.vvm.sh.baseDados.entidades;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "tiposTemplateAvrLevantamentos")
+public class TipoTemplateAvrLevantamento {
+
+    @NonNull
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @NonNull
+    public int idModelo;
+
+
+    @NonNull
+    public String servId;
+
+
+    @NonNull
+    public String tarefa;
+
+
+    @NonNull
+    public String perigo;
+
+    @NonNull
+    @ColumnInfo(name = "ativo")
+    public int ativo;
+}

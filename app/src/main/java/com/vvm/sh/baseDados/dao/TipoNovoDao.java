141e7bcd1fb669f2bdd085526f603ee4e2a825cd
gustavo <gustavo.maia@b2b.com.pt>
2020-10-01 13:34:27 +0100

    TipoNovoDao

---
+++ app/src/main/java/com/vvm/sh/baseDados/dao/TipoNovoDao.java
@@ -0,0 +1,13 @@
+package com.vvm.sh.baseDados.dao;
+
+import androidx.room.Dao;
+
+import com.vvm.sh.baseDados.BaseDao;
+import com.vvm.sh.baseDados.entidades.TipoNovo;
+
+@Dao
+abstract public class TipoNovoDao implements BaseDao<TipoNovo> {
+
+
+
+}

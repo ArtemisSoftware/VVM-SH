574226374eb56e884308d82173ba80ec5b9cd2ce
gustavo <gustavo.maia@b2b.com.pt>
2020-10-22 11:44:39 +0100

    ImagensViewModelsModule

---
+++ app/src/main/java/com/vvm/sh/di/imagens/ImagensViewModelsModule.java
@@ -0,0 +1,20 @@
+package com.vvm.sh.di.imagens;
+
+import androidx.lifecycle.ViewModel;
+
+import com.vvm.sh.di.ViewModelKey;
+import com.vvm.sh.ui.atividadesPendentes.relatorios.FormacaoViewModel;
+import com.vvm.sh.ui.imagens.ImagemViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class ImagensViewModelsModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ImagemViewModel.class)
+    public abstract ViewModel bindImagemViewModel(ImagemViewModel viewModel);
+}

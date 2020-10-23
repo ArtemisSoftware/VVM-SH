eda123e00c3a88cdcb0d0fce72ed1ef4ec151d0c
gustavo <gustavo.maia@b2b.com.pt>
2020-10-23 11:09:57 +0100

    Classe de envio RegistoVisita

---
+++ app/src/main/java/com/vvm/sh/api/modelos/envio/RegistoVisita.java
@@ -0,0 +1,28 @@
+package com.vvm.sh.api.modelos.envio;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegistoVisita {
+
+    @SerializedName("recebidoPor")
+    public String recebidoPor;
+
+    @SerializedName("funcao")
+    public String funcao;
+
+    @SerializedName("obs")
+    public String observacao;
+
+    @SerializedName("trabalhosRealizados")
+    public List<TrabalhoRealizado> trabalhosRealizados;
+
+    @SerializedName("Album")
+    public ArrayList<String> album;
+
+    @SerializedName("data")
+    public String data;
+
+}

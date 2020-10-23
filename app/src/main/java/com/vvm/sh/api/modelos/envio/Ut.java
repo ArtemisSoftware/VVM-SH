fdb8a4f6d1d9d5dad540bfed1078a172b7f8e91e
gustavo <gustavo.maia@b2b.com.pt>
2020-10-23 13:09:51 +0100

    Classe de envio - Ut

---
+++ app/src/main/java/com/vvm/sh/api/modelos/envio/Ut.java
@@ -0,0 +1,76 @@
+package com.vvm.sh.api.modelos.envio;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Ut {
+
+
+    @SerializedName("idItem")
+    public String idItem;
+
+    //ut1
+
+    @SerializedName("idUT1")
+    public String idUT1;
+
+
+    @SerializedName("idCategoriasRiscoUT_1")
+    public String idCategoriasRiscoUT_1;
+
+//			if(categoriasRiscoUt1== null){
+//        categoriasRiscoUt1 = AppIF.SEM_TEXTO;
+//    }
+
+
+    @SerializedName("localRiscoA_ut_1")
+    public boolean localRiscoA_ut_1;
+
+    @SerializedName("localRiscoB_ut_1")
+    public boolean localRiscoB_ut_1;
+
+    @SerializedName("localRiscoC_ut_1")
+    public boolean localRiscoC_ut_1;
+
+    @SerializedName("localRiscoD_ut_1")
+    public boolean localRiscoD_ut_1;
+
+    @SerializedName("localRiscoE_ut_1")
+    public boolean localRiscoE_ut_1;
+
+    @SerializedName("localRiscoF_ut_1")
+    public boolean localRiscoF_ut_1;
+
+
+    //ut2
+
+    @SerializedName("idUT2")
+    public String idUT2;
+
+    @SerializedName("idCategoriasRiscoUT_2")
+    public String idCategoriasRiscoUT_2;
+
+
+//			if(categoriasRiscoUt2== null){
+//        categoriasRiscoUt2 = AppIF.SEM_TEXTO;
+//    }
+
+
+    @SerializedName("localRiscoA_ut_2")
+    public boolean localRiscoA_ut_2;
+
+    @SerializedName("localRiscoB_ut_2")
+    public boolean localRiscoB_ut_2;
+
+    @SerializedName("localRiscoC_ut_2")
+    public boolean localRiscoC_ut_2;
+
+    @SerializedName("localRiscoD_ut_2")
+    public boolean localRiscoD_ut_2;
+
+    @SerializedName("localRiscoE_ut_2")
+    public boolean localRiscoE_ut_2;
+
+    @SerializedName("localRiscoF_ut_2")
+    public boolean localRiscoF_ut_2;
+
+}

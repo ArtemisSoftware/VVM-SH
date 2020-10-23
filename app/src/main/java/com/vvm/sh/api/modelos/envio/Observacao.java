f0d4360b9ad69652e0291be9cf6305b85417c66e
gustavo <gustavo.maia@b2b.com.pt>
2020-10-23 13:05:01 +0100

    Classe de envio - Observacao

---
+++ app/src/main/java/com/vvm/sh/api/modelos/envio/Observacao.java
@@ -0,0 +1,12 @@
+package com.vvm.sh.api.modelos.envio;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Observacao {
+
+    @SerializedName("idItem")
+    public String idItem;
+
+    @SerializedName("resposta")
+    public String resposta;
+}

c986ba1e62db50767d06263abaf0d50dc8c61c3c
gustavo <gustavo.isaac@gmail.com>
2021-01-07 09:06:28 +0000

    ObrigacaoLegalViewHolder

---
+++ app/src/main/java/com/vvm/sh/ui/informacaoSst/adaptadores/ObrigacaoLegalViewHolder.java
@@ -0,0 +1,33 @@
+package com.vvm.sh.ui.informacaoSst.adaptadores;
+
+import android.view.View;
+import android.widget.CheckBox;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vvm.sh.databinding.ItemObrigacaoLegalBinding;
+import com.vvm.sh.databinding.ItemTrabalhoRealizadoBinding;
+import com.vvm.sh.ui.registoVisita.adaptadores.OnRegistoVisitaListener;
+
+public class ObrigacaoLegalViewHolder extends RecyclerView.ViewHolder implements CheckBox.OnClickListener {
+
+
+    ItemObrigacaoLegalBinding binding;
+    private OnInformacaoSstListener onItemListener;
+
+
+    public ObrigacaoLegalViewHolder(View itemView, OnInformacaoSstListener onItemListener) {
+        super(itemView);
+        binding = DataBindingUtil.bind(itemView);
+
+        this.onItemListener = onItemListener;
+        binding.chkBoxItem.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+       //--onItemListener.onItemChecked(binding.getTrabalho().tipo.id, ((CheckBox) v).isChecked());
+    }
+}

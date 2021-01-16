7a5ffd95c5779d6f839dc973d46a0f217fbb2bc6
gustavo <gustavo.isaac@gmail.com>
2021-01-16 20:10:07 +0000

    Dependecia Ftp

---
+++ app/src/main/java/com/vvm/sh/util/constantes/FtpConfig.java
@@ -0,0 +1,9 @@
+package com.vvm.sh.util.constantes;
+
+public class FtpConfig {
+
+    public static final String SERVIDOR = "ftp.dlptest.com";
+    public static final int PORTO = 21;
+    public static final String UTILIZADOR = "dlpuser";
+    public static final String PALAVRA_CHAVE = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+}

9613b445c7aa44f458a0939833c76222d0f7ef6a
gustavo <gustavo.isaac@gmail.com>
2021-01-20 18:27:43 +0000

    evento pdf - CabecalhoRodape

--- app/src/main/java/com/vvm/sh/documentos/certificadoTratamento/eventos/CabecalhoRodape.java
+++ app/src/main/java/com/vvm/sh/documentos/certificadoTratamento/eventos/CabecalhoRodape.java
@@ -1,6 +1,36 @@
 package com.vvm.sh.documentos.certificadoTratamento.eventos;
 
+import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.PdfPageEventHelper;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class CabecalhoRodape extends PdfPageEventHelper {
+
+    private Cabecalho cabecalho;
+
+
+    public CabecalhoRodape(){
+
+        cabecalho = new Cabecalho();
+    }
+
+
+    private void fixarCabecalho(PdfWriter writer, Document document){
+
+        float height = 0;
+
+        cabecalho.getSection().getPdfTable().writeSelectedRows(0, -1, document.left(), document.top() + ((document.topMargin() + height) / 2), writer.getDirectContent());
+    }
+
+
+
+    @Override
+    public void onEndPage(PdfWriter writer, Document document) {
+
+        fixarCabecalho(writer, document);
+    }
+
+
+
+
 }

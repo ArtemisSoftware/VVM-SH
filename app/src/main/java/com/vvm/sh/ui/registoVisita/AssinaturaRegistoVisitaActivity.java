d8fb294f25ebbbeeabb64a689c9b85fb9446bdc2
gustavo <gustavo.isaac@gmail.com>
2021-02-22 13:03:42 +0000

    Layout activity_assinatura_registo_visita.xml

---
+++ app/src/main/java/com/vvm/sh/ui/registoVisita/AssinaturaRegistoVisitaActivity.java
@@ -0,0 +1,16 @@
+package com.vvm.sh.ui.registoVisita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvm.sh.R;
+
+public class AssinaturaRegistoVisitaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assinatura_registo_visita);
+    }
+}
